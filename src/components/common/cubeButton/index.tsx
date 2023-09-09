d6af0b8c5b1b7839806165bfec4b4eb5ce16cf76
cloud0406 <cloud0406@naver.com>
2023-09-09 16:28:33 +0900

    style: cubeButton 컴포넌트 작업

---
+++ src/components/common/cubeButton/index.tsx
@@ -0,0 +1,60 @@
+import { Text } from '@/components/common/Text'
+import { KeyOfPalette, KeyOfTypo, theme } from '@/styles/theme'
+import styled from '@emotion/styled'
+import { ComponentProps } from 'react'
+import { Link } from 'react-router-dom'
+
+interface CubeButtonProps extends ComponentProps<'div'> {
+  content: string
+  path: string
+  textTypo?: KeyOfTypo
+  textColor?: KeyOfPalette
+  backgroundColor?: KeyOfPalette
+  shadowColor?: KeyOfPalette
+}
+
+/**
+ * @param content : 채널에 대한 설명
+ * @param path : 클릭시 이동 경로 지정
+ * @param textTypo : content에 적용할 typo
+ * @param textColor: content에 적용시킬 color
+ * @param backgroundColor : 카드 element에 적용할 배경 color
+ * @param shadowColor : 카드 element에 적용할 shadow color
+ */
+
+const CubeButton = ({
+  content,
+  path,
+  textTypo = 'SubHead_18',
+  textColor = 'BLACK',
+  backgroundColor = 'WHITE',
+  shadowColor = 'GRAY400',
+}: CubeButtonProps) => {
+  return (
+    <CubeButtonWrapper content={content} backgroundColor={backgroundColor} shadowColor={shadowColor}>
+      <Link to={path} style={{ textDecoration: 'none' }}>
+        <TextWrapper>
+          <Text typo={textTypo} color={textColor}>
+            {content}
+          </Text>
+        </TextWrapper>
+      </Link>
+    </CubeButtonWrapper>
+  )
+}
+
+const CubeButtonWrapper = styled.div<{ shadowColor?: KeyOfPalette; backgroundColor?: KeyOfPalette }>`
+  width: 160px;
+  height: 86px;
+  border-radius: 10px;
+  background-color: ${({ backgroundColor }) => backgroundColor && theme.palette[backgroundColor]};
+  box-shadow: ${({ shadowColor }) => shadowColor && `0px 4px 4px 0px ${theme.palette[shadowColor]}`};
+  word-break: keep-all;
+`
+
+const TextWrapper = styled.div`
+  padding: 19px 36px 16px 15px;
+  height: 100%;
+`
+
+export default CubeButton
