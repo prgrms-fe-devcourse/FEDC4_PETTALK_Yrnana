e2d0c872eb0adf0408538ad9bdc6247b7e1dd8ea
cloud0406 <cloud0406@naver.com>
2023-09-10 01:25:01 +0900

    chore: vite-plugin-svgr 플러그인 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '.',
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [
       { find: '@/pages', replacement: path.resolve(__dirname, 'src/pages') },
