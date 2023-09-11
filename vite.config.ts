964e24bad9e7f8128cb204b5eaec61472abd24a6
cloud0406 <cloud0406@naver.com>
2023-09-11 17:39:18 +0900

    chore: 불필요 플러그인 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '.',
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: { '@/': `${process.cwd()}/src/` },
   },
