d1dde8dd0b4a65a377c7b4080094ae24927b0714
Eugene Kim <67894159+eugene028@users.noreply.github.com>
2023-09-13 17:51:32 +0900

    fix : axios response 모듈 수정 (#51)
    - 정호님 담당인 Channel 페이지도 조금 useQuery 이용해서 손봐봤어영

--- src/libs/apis/channel/ChannelApi.ts
+++ src/libs/apis/channel/ChannelApi.ts
@@ -3,7 +3,7 @@ import { Channel, ChannelCreateRequest } from '@/libs/apis/channel/channelType'
 export const ChannelApi = {
   CREATE_CHANNEL: async (payload: ChannelCreateRequest) => {
     const response = await axiosAPI.post('/channels/create', payload)
-    return response.data
+    return response
   },
   GET_CHANNEL: async (): Promise<Channel[]> => {
     const response = await axiosAPI.get('/channels')
--- src/pages/home/MainPage.tsx
+++ src/pages/home/MainPage.tsx
@@ -3,7 +3,10 @@ import { useQuery } from '@tanstack/react-query'
 import { ChannelApi } from '@/libs/apis/channel/ChannelApi'
 
 const MainPage = () => {
-  //const { data, isLoading } = useQuery('channels', ChannelApi.GET_CHANNEL)
-  return <>{'채널명이 들어갈 메인 페이지입니다.'}</>
+  const { data, isLoading } = useQuery(['channels'], () => ChannelApi.GET_CHANNEL())
+
+  //TODO: 로딩 컴포넌트 추가 시 리팩토링 가능성 있음
+  if (isLoading) return <h2>{'Loading중...'}</h2>
+  return <>{data?.map((channel, index) => <li key={index}>{channel.name}</li>)}</>
 }
 export default MainPage
