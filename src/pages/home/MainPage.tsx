65c8caacd3373ff13f2c8f81d9ed23edcda3a5b6
cloud0406 <cloud0406@naver.com>
2023-09-14 02:42:53 +0900

    feat: 실제 채널 데이터 받기

--- src/pages/home/MainPage.tsx
+++ src/pages/home/MainPage.tsx
@@ -24,10 +24,6 @@ const MainPage = ({
   interestChannelColor = 'BLACK',
   ...props
 }: MainPageProps) => {
-  const { data, isLoading } = useQuery(['channels'], () => ChannelApi.GET_CHANNEL())
-
-  //TODO: 로딩 컴포넌트 추가 시 리팩토링 가능성 있음
-  if (isLoading) return <h2>{'Loading중...'}</h2>
   return (
     <MainPageWrapper {...props}>
       <TodayChannel>
--- src/pages/home/channelList/index.tsx
+++ src/pages/home/channelList/index.tsx
@@ -1,41 +1,37 @@
 import styled from '@emotion/styled'
+import { useQuery } from '@tanstack/react-query'
 
 import CubeButton from '@/components/common/cubeButton'
 import { FlexBox } from '@/components/common/flexBox'
 import Spacing from '@/components/common/spacing'
+import { ChannelApi } from '@/libs/apis/channel/ChannelApi'
 
 import { channelMock } from '../../../mock/channel'
 
 const ChannelList = () => {
+  const { data, isLoading } = useQuery(['channels'], () => ChannelApi.GET_CHANNEL())
+
+  console.log(data)
+  if (isLoading) return <h2>{'로딩 중...'}</h2>
+
   return (
     <ChannelWrapper>
-      {channelMock &&
-        channelMock.map((channel, index) => {
-          if (index % 2 === 0) {
-            const nextChannel = channelMock[index + 1]
-
-            return (
-              <div key={channel._id}>
-                <FlexBox gap={13}>
-                  <CubeButton content={channel.name} path={`/${channel._id}`}></CubeButton>
-                  {nextChannel && (
-                    <CubeButton
-                      content={nextChannel.name}
-                      path={`/${nextChannel._id}`}
-                    ></CubeButton>
-                  )}
-                </FlexBox>
-                <Spacing size={13}></Spacing>
-              </div>
-            )
-          }
-
-          return null
+      {data &&
+        data.map((channel) => {
+          return (
+            <FlexBox key={channel._id}>
+              <CubeButton content={channel.name} path={`/post/${channel._id}`}></CubeButton>
+            </FlexBox>
+          )
         })}
     </ChannelWrapper>
   )
 }
 
-const ChannelWrapper = styled.div``
+const ChannelWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 15px;
+`
 
 export default ChannelList
