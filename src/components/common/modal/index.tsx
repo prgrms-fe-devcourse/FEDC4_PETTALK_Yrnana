58e4dd0e950d2152cc4a3832a38c07540ff7639c
judahhh <jdh99080516@gmail.com>
2023-09-13 20:49:48 +0900

    style : 알림용 모달창 디자인

--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -1,67 +1,29 @@
-import styled from '@emotion/styled'
-import { Dispatch, SetStateAction } from 'react'
+import 'react-toastify/dist/ReactToastify.css'
 
-import { palette } from '@/styles/palette'
+import { useEffect } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
+
 interface ModalProps {
-  setModalState: Dispatch<SetStateAction<boolean>>
-  modalState: boolean
-  value: string
-  width?: number
-  height?: number
+  modalText: string
+  time: number
 }
-
-const Modal = ({ setModalState, modalState, value, width = 500, height = 400 }: ModalProps) => {
-  const onClickCloseButton = () => {
-    setModalState(!modalState)
-  }
+const Modal = ({ modalText, time }: ModalProps) => {
+  const notify = () => toast(modalText)
+  useEffect(() => {
+    notify()
+  }, [])
   return (
-    <StyleModalContainer>
-      <DialogBox>
-        {value}
-        <button onClick={onClickCloseButton}>{'끄기'}</button>
-      </DialogBox>
-      <Backdrop
-        onClick={(e: React.MouseEvent) => {
-          e.preventDefault()
-        }}
+    <>
+      <ToastContainer
+        position={'top-center'} // 알람 위치 지정
+        autoClose={time} // 자동 off 시간
+        hideProgressBar={true} // 진행시간바 숨김
+        closeOnClick // 클릭으로 알람 닫기
+        theme={'light'}
+        limit={1} // 알람 개수 제한
       />
-    </StyleModalContainer>
+    </>
   )
 }
-
-const StyleModalContainer = styled.div`
-  background-color: rgba(${palette.BLACK}, 0.5);
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
-const DialogBox = styled.dialog`
-  width: 600px;
-  height: 400px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border: none;
-  border-radius: 10px;
-  box-shadow: 0 0 30px rgba(30, 30, 30, 0.185);
-  box-sizing: border-box;
-  background-color: ${palette.WHITE};
-  z-index: 10000;
-`
-
-const Backdrop = styled.div`
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  top: 0;
-  z-index: 9999;
-  background-color: rgba(0, 0, 0, 0.2);
-`
 
 export default Modal
