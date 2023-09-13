b1ad255e6b165dff9f301029a70ff3edc6473a49
judahhh <jdh99080516@gmail.com>
2023-09-13 21:30:13 +0900

    style : 모달창 디자인 커스터마이징

--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -1,7 +1,11 @@
 import 'react-toastify/dist/ReactToastify.css'
 
+import styled from '@emotion/styled'
 import { useEffect } from 'react'
 import { toast, ToastContainer } from 'react-toastify'
+
+import { palette } from '@/styles/palette'
+import { theme } from '@/styles/theme'
 
 interface ModalProps {
   modalText: string
@@ -14,16 +18,23 @@ const Modal = ({ modalText, time }: ModalProps) => {
   }, [])
   return (
     <>
-      <ToastContainer
+      <StyleModal
         position={'top-center'} // 알람 위치 지정
         autoClose={time} // 자동 off 시간
         hideProgressBar={true} // 진행시간바 숨김
         closeOnClick // 클릭으로 알람 닫기
-        theme={'light'}
+        pauseOnHover={false}
         limit={1} // 알람 개수 제한
       />
     </>
   )
 }
-
+const StyleModal = styled(ToastContainer)`
+  .Toastify__toast {
+    font-size: ${theme.typo.Body_13};
+    border-radius: 10px;
+    padding: 16px 20px;
+    color: ${palette.GRAY600};
+  }
+`
 export default Modal
