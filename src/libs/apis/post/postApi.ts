5debfbc890e3eccd4d6835d3623ee6630f42490f
Eugene Kim <eugene02876@gmail.com>
2023-09-14 23:59:54 +0900

    chore : api response 객체 생성

--- src/libs/apis/post/postApi.ts
+++ src/libs/apis/post/postApi.ts
@@ -1,15 +1,14 @@
 import { axiosAPI } from '@/libs/apis/axios'
 import { CreatePostRequest } from '@/libs/apis/post/postType'
-
+import { Post } from '@/libs/apis/post/postType'
 const accessToken =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0ZWRiYTQzMDdjZDhmMTIxNjJlMmVhYSIsImVtYWlsIjoiYWRtaW5AcHJvZ3JhbW1lcnMuY28ua3IifSwiaWF0IjoxNjk0NjgxODE4fQ.Zfza6chsiXM7cZzHLsJYfWTk7TeOtTP6eSrrxX62vEY'
 
 const PostApi = {
-  CREATE_POST: async (payload: CreatePostRequest) => {
+  CREATE_POST: async (payload: CreatePostRequest): Promise<Post> => {
     axiosAPI.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
-    const data = await axiosAPI.post('posts/create', payload)
-    console.log(data)
-    return data
+    const response = await axiosAPI.post('posts/create', payload)
+    return response.data
   },
 }
 
