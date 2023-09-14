41bfa8861286ed4e3e6983a8de009db4a3df2410
judahhh <jdh99080516@gmail.com>
2023-09-14 17:27:54 +0900

    feat : 로그인 한 사용자와 안 한 사용자 인가 처리

--- src/pages/redirect/Redirect.tsx
+++ src/pages/redirect/Redirect.tsx
@@ -2,7 +2,6 @@ import { Navigate } from 'react-router-dom'
 
 const RedirectPage = () => {
   //여기서 로그인 여부를 boolean으로 받아 확인
-  //   -> localStorage, jotai Store중 어디에 로그인 여부를 저장할지 고민
   const isLogin = localStorage.getItem('loginState')
 
   return <div>{!isLogin && <Navigate to={'/login'}></Navigate>}</div>
