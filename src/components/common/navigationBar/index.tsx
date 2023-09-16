ff4abb001d9fafc78d0a20e4a855a39f5dc3878b
cloud0406 <cloud0406@naver.com>
2023-09-17 00:00:39 +0900

    style: flex 영역 flexbox 컴포넌트로 처리

--- src/components/common/navigationBar/index.tsx
+++ src/components/common/navigationBar/index.tsx
@@ -5,6 +5,7 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import Chat from '@/assets/icons/Chat'
 import Follow from '@/assets/icons/Follow'
 import Shop from '@/assets/icons/Shop'
+import { FlexBox } from '@/components/common/flexBox'
 import { palette } from '@/styles/palette'
 import { typo } from '@/styles/typo'
 
@@ -19,72 +20,62 @@ const NavigationBar = () => {
     navigate(`/${path}`)
   }
   return (
-    <StyleWrapper>
-      <StyleNavigation>
+    <StyleWrapper fullWidth={true}>
+      <StyleNavigation justify={'space-around'}>
         <StyleNavigationItem onClick={() => moveFromNavigationBar('chatting')}>
-          {/* 추후 StyleIcon 위치에 해당 아이콘 넣을 예정입니다. */}
-          <StyleIcon clickMenu={clickMenu == 'chatting'}>
-            <Chat />
-          </StyleIcon>
-          <StyleNavigationText>{'채팅목록'}</StyleNavigationText>
+          <FlexBox direction={'column'} gap={5}>
+            <StyleIcon clickMenu={clickMenu == 'chatting'}>
+              <Chat />
+            </StyleIcon>
+            <StyleNavigationText>{'채팅목록'}</StyleNavigationText>
+          </FlexBox>
         </StyleNavigationItem>
         <StyleNavigationItem onClick={() => moveFromNavigationBar('')}>
-          <StyleIcon clickMenu={clickMenu == ''}>
-            <Shop />
-          </StyleIcon>
-          <StyleNavigationText>{'채널탐색'}</StyleNavigationText>
+          <FlexBox direction={'column'} gap={5}>
+            <StyleIcon clickMenu={clickMenu == ''}>
+              <Shop />
+            </StyleIcon>
+            <StyleNavigationText>{'채널탐색'}</StyleNavigationText>
+          </FlexBox>
         </StyleNavigationItem>
         <StyleNavigationItem onClick={() => moveFromNavigationBar('friends')}>
-          <StyleIcon clickMenu={clickMenu == 'friends'}>
-            <Follow height={25} />
-          </StyleIcon>
-          <StyleNavigationText>{'팔로우목록'}</StyleNavigationText>
+          <FlexBox direction={'column'} gap={5}>
+            <StyleIcon clickMenu={clickMenu == 'friends'}>
+              <Follow height={25} />
+            </StyleIcon>
+            <StyleNavigationText>{'팔로우목록'}</StyleNavigationText>
+          </FlexBox>
         </StyleNavigationItem>
       </StyleNavigation>
     </StyleWrapper>
   )
 }
-const StyleWrapper = styled.div`
-  display: flex;
-  justify-content: center;
+const StyleWrapper = styled(FlexBox)`
   position: absolute;
   bottom: 30px;
-  width: 100%;
-  margin: auto 0;
 `
-const StyleNavigation = styled.div`
+
+const StyleNavigation = styled(FlexBox)`
   width: 225px;
   height: 59px;
   background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
   border-radius: 30px;
-  padding-left: 15px;
-  padding-right: 15px;
+  padding: 0 15px;
   box-shadow:
     0px 0px 2px 0px rgba(0, 0, 0, 0.24),
     0px 4px 4px 0px rgba(0, 0, 0, 0.14);
 `
-const StyleNavigationItem = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 5px;
-`
+
+const StyleNavigationItem = styled.button``
 
 const StyleNavigationText = styled.span`
   font-size: ${typo.Caption_9};
 `
-const StyleIcon = styled.span<{ clickMenu: boolean }>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 28px;
-  height: 28px;
+
+const StyleIcon = styled(FlexBox)<{ clickMenu: boolean }>`
+  width: 32px;
+  height: 32px;
   background-color: ${({ clickMenu }) => (clickMenu ? `${palette.CORAL}` : '')};
   border-radius: 50px;
-  /* background-color: ${palette.CORAL}; */
 `
 export default NavigationBar
