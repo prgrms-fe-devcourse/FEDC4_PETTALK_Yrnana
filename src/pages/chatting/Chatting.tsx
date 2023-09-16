3ecec249c1fd875f9724fd2895b912d734562482
cloud0406 <cloud0406@naver.com>
2023-09-16 23:13:34 +0900

    style: 채팅창 페이지 레이아웃 구현

--- src/pages/chatting/Chatting.tsx
+++ src/pages/chatting/Chatting.tsx
@@ -1,5 +1,75 @@
+import ChattingBubble from '@/components/common/chattingBubble'
+import { FlexBox } from '@/components/common/flexBox'
+import ListRow from '@/components/common/listRow'
+import ProfileImage from '@/components/common/profileImage'
+import Spacing from '@/components/common/spacing'
+import { Message } from '@/libs/apis/message/messageType'
+import Datetime from '@/pages/chatting/datetime'
+
+import { messageMock } from '../../mock/message'
+
+interface FormattedMessage extends Message {
+  formattedDate: string
+}
+
 const Chatting = () => {
-  return <div>{'chatting 페이지입니다~'}</div>
+  const parsedMessages: FormattedMessage[] = messageMock.map((message) => {
+    const parsedDate = new Date(message.createdAt)
+    const formattedDate = `${parsedDate.getFullYear()}년 ${
+      parsedDate.getMonth() + 1
+    }월 ${parsedDate.getDate()}일`
+    return { ...message, formattedDate }
+  })
+
+  const groupedMessages: Record<string, FormattedMessage[]> = {}
+  parsedMessages.forEach((message) => {
+    const date = message.formattedDate
+    if (!groupedMessages[date]) {
+      groupedMessages[date] = []
+    }
+    groupedMessages[date].push(message)
+  })
+
+  // 결과 출력
+  console.log('일자별 그룹화된 메시지:')
+  console.log(groupedMessages)
+  for (const [key, value] of Object.entries(groupedMessages)) {
+    console.log('Date:', key)
+    console.log('Messages:', value)
+  }
+
+  const entries = Object.entries(groupedMessages)
+
+  return (
+    <>
+      {entries.map(([date, messages]) => (
+        <FlexBox direction={'column'} gap={20} key={date}>
+          <Datetime content={date}></Datetime>
+          {messages.map((message) =>
+            'id2' === message.sender._id ? (
+              <ChattingBubble
+                key={message._id}
+                isMyChat={true}
+                message={message.message}
+                time={'17:45'}
+              />
+            ) : (
+              <ListRow
+                key={message._id}
+                leftImage={
+                  <ProfileImage size={35} image={message.sender.image} updatable={false} />
+                }
+                mainText={message.sender.fullName}
+                subElement={<ChattingBubble message={message.message} time={'17:44'} />}
+                rightElement={null}
+              />
+            ),
+          )}
+          <Spacing size={20}></Spacing>
+        </FlexBox>
+      ))}
+    </>
+  )
 }
 
 export default Chatting
