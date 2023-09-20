5f9b30d59fb053a3b91d4374e05897205f83e8cf
cloud0406 <cloud0406@naver.com>
2023-09-20 22:47:27 +0900

    feat: 사용자별 다른 채팅방 입장

--- src/pages/chatting/Chatting.tsx
+++ src/pages/chatting/Chatting.tsx
@@ -1,23 +1,22 @@
 import styled from '@emotion/styled'
 import { useMutation, useQuery } from '@tanstack/react-query'
-import { useAtom, useAtomValue } from 'jotai'
+import { useAtomValue } from 'jotai'
 import { MouseEvent, useEffect, useRef, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 import Button from '@/components/common/button'
 import ChattingBubble from '@/components/common/chattingBubble'
 import { FlexBox } from '@/components/common/flexBox'
 import ListRow from '@/components/common/listRow'
 import Loading from '@/components/common/loading'
-import ProfileImage from '@/components/common/profileImage'
 import Spacing from '@/components/common/spacing'
 import TextArea from '@/components/common/textarea'
+import { User } from '@/libs/apis/auth/authType'
 import MessageApi from '@/libs/apis/message/messageApi'
 import { Message } from '@/libs/apis/message/messageType'
 import { userAtom } from '@/libs/store/userAtom'
 import Datetime from '@/pages/chatting/datetime'
 import { theme } from '@/styles/theme'
-
-import { messageMock } from '../../mock/message'
 
 interface FormattedMessage extends Message {
   formattedDate: string
@@ -26,42 +25,36 @@ interface FormattedMessage extends Message {
 
 const Chatting = () => {
   const userData = useAtomValue(userAtom)
-
   const messageRef = useRef<HTMLTextAreaElement>(null)
-
-  const [testUserId, setTestUserId] = useState<string>('')
-
-  useEffect(() => {
-    setTestUserId(
-      userData._id === '65029272aa40045f5cff7ae9'
-        ? '6509302b2b2dc04dc8b34182'
-        : '65029272aa40045f5cff7ae9',
-    )
-  }, [userData])
+  const [opponent, setOpponent] = useState<string>('')
+  const { sender, receiver } = (useLocation().state || {}) as { sender: User; receiver: User }
+  const messageWrapperRef = useRef<HTMLDivElement>(null)
+  const [isInitialRender, setIsInitialRender] = useState(true)
+  const [messageData, setMessageData] = useState<Message[] | []>([])
 
   const getDetailMessages = async (userId: string) => {
-    if (!userId) return
-
-    const response = await MessageApi.GET_DETAIL_MESSAGES(userId)
-    return response
+    try {
+      if (!userId) return []
+      const response = await MessageApi.GET_DETAIL_MESSAGES(userId)
+      return response
+    } catch (error) {
+      console.error('Error fetching messages:', error)
+      throw error
+    }
   }
 
-  const { data, error, isLoading, refetch } = useQuery(
-    ['messages'],
-    () => getDetailMessages(testUserId),
-    {
-      enabled: testUserId !== userData._id, // testUserId가 존재할 때만 호출
-      refetchInterval: 5000,
-      refetchIntervalInBackground: true,
-      retry: 3,
+  const { data, isLoading } = useQuery(['messages', opponent], () => getDetailMessages(opponent), {
+    enabled: opponent !== userData._id,
+    refetchInterval: 2000,
+    refetchIntervalInBackground: true,
+    retry: 3,
+    onSuccess: (responseData) => {
+      setMessageData(responseData)
     },
-  )
-  console.log(data)
+  })
 
   const mutation = useMutation(MessageApi.SEND_MESSAGE, {
     onSuccess: () => {
-      console.log('메시지 전송 성공!')
-
       if (messageRef.current) messageRef.current.value = ''
     },
     onError: (error) => {
@@ -71,25 +64,18 @@ const Chatting = () => {
 
   const sendMessage = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
-
-    if (!messageRef.current || !messageRef.current.value) {
-      console.log('메시지를 입력하세요.')
-      return
-    }
+    if (!messageRef.current || !messageRef.current.value) return
 
     try {
       await mutation.mutateAsync({
         message: messageRef.current.value,
-        receiver: testUserId,
+        receiver: opponent,
       })
     } catch (error) {
       console.error('메시지 전송 중 오류 발생:', error)
     }
   }
 
-  // ----  데이터 포맷팅 ------
-
-  // utc 시간, 날짜 포맷팅
   const parsedMessages: FormattedMessage[] = data
     ? data.map((message) => {
         const parsedDate = new Date(message.createdAt)
@@ -104,29 +90,25 @@ const Chatting = () => {
       })
     : []
 
-  // 날짜 기준 메시지 그룹화
   const groupedMessages: Record<string, FormattedMessage[]> = {}
   parsedMessages.forEach((message) => {
     const date = message.formattedDate
     if (!groupedMessages[date]) groupedMessages[date] = []
     groupedMessages[date].push(message)
   })
-
-  // 객체 -> 배열
   const entries = Object.entries(groupedMessages)
 
-  const messageWrapperRef = useRef<HTMLDivElement>(null)
-  const [isInitialRender, setIsInitialRender] = useState(true)
+  useEffect(() => {
+    setOpponent(sender._id === userData._id ? receiver._id : sender._id)
+  }, [userData])
 
   useEffect(() => {
     // 최초 렌더링 시에만 스크롤을 가장 아래로 이동
-    if (isInitialRender && data && messageWrapperRef.current) {
+    if (isInitialRender && messageData.length > 0 && messageWrapperRef.current) {
       messageWrapperRef.current.scrollTop = messageWrapperRef.current.scrollHeight
-      setIsInitialRender(false) // 최초 렌더링 이후로는 더 이상 실행하지 않음
+      setIsInitialRender(false)
     }
-  }, [data, isInitialRender]) // data 또는 isInitialRender가 업데이트될 때마다 실행
-
-  // ... (기존 코드)
+  }, [isInitialRender, messageData])
 
   return (
     <ChattingWrapper direction={'column'} fullWidth={true} align={'stretch'}>
@@ -188,6 +170,7 @@ const MessageWrapper = styled.div`
   max-height: calc(100% - 70px);
   flex: 1;
   overflow-y: auto;
+  scroll-behavior: smooth;
 `
 
 const TypingFlexBox = styled(FlexBox)`
