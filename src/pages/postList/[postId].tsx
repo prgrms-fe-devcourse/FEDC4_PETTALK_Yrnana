8278ba0ce0b96f56f2a15641b02b58a07c2b559b
judahhh <jdh99080516@gmail.com>
2023-09-22 17:42:17 +0900

    fix : 자신의 게시물에 댓글 달거나 좋아요 눌렀을 떄 알림 전송 안 되게 수정

--- src/pages/postList/[postId].tsx
+++ src/pages/postList/[postId].tsx
@@ -36,12 +36,14 @@ const PostDetailPage = () => {
     },
     onSuccess: (like: Like) => {
       setUserData({ ...userData, likes: [...userData.likes, like] })
-      useNotification({
-        postId: postId,
-        userId: data !== undefined ? data?.author._id : '',
-        type: 'LIKE',
-        typeId: like._id,
-      })
+      data?.author._id !== userData._id
+        ? useNotification({
+            postId: postId,
+            userId: data !== undefined ? data?.author._id : '',
+            type: 'LIKE',
+            typeId: like._id,
+          })
+        : ''
     },
   })
   const unlikeMutation = useMutation(PostApi.UNLIKE_POST, {
@@ -120,12 +122,14 @@ const PostDetailPage = () => {
         postId: postId,
       })
       refetch()
-      useNotification({
-        postId: postId,
-        userId: data !== undefined ? data?.author._id : '',
-        type: 'COMMENT',
-        typeId: response.data._id,
-      })
+      data?.author._id !== userData._id
+        ? useNotification({
+            postId: postId,
+            userId: data !== undefined ? data?.author._id : '',
+            type: 'COMMENT',
+            typeId: response.data._id,
+          })
+        : ''
       return response
     } else {
       alert('댓글을 입력해주세요!')
