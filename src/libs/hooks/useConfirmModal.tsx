dd54a0bb05ef87e3e9e2064da7a9d10e5d0b1189
DaHyeonJu <jdh99080516@gmail.com>
2023-09-25 13:58:32 +0900

    [Feature] confirm 모달창 추가 (#122)
    * feature : 사용자에게 한 번 더 확인받을 수 있는
    confirm 모달창 추가
    
    * fix : confirm modal 상태를 전역에서 관리하도록 수정

---
+++ src/libs/hooks/useConfirmModal.tsx
@@ -0,0 +1,25 @@
+import { useAtom, useAtomValue, useSetAtom } from 'jotai'
+
+import ModalConfirm from '@/components/common/modalConfirm'
+import {
+  modalConfirmAtom,
+  modalConfirmFuncAtom,
+  modalConfirmTextAtom,
+} from '@/libs/store/modalConfirmAtom'
+interface ModalConfirmPropsType {
+  okFunc: () => void
+  confirmText: string
+}
+export const useConfirmModal = () => {
+  const setModalState = useSetAtom(modalConfirmAtom)
+  const setModalText = useSetAtom(modalConfirmTextAtom)
+  const [modalFunc, setModalFunc] = useAtom(modalConfirmFuncAtom)
+
+  const openConfirmModal = ({ confirmText, okFunc }: ModalConfirmPropsType) => {
+    setModalState(true)
+    setModalText(confirmText)
+    setModalFunc({ fn: okFunc })
+  }
+
+  return { openConfirmModal, ModalConfirm }
+}
