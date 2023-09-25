189140433630eb6f5a5a7bbcc2e5dc2f1522a562
JeongWuk_99 <61570018+JeongWuk@users.noreply.github.com>
2023-09-25 14:17:44 +0900

    [Style] : 페이지 배경 이미지 적용 및 404페이지 적용 (#130)
    * style : 페이지 배경 이미지 적용 및 404페이지 적용
    
    * chore : 충돌 해결

--- src/components/layouts/AppBarNavTemplate.tsx
+++ src/components/layouts/AppBarNavTemplate.tsx
@@ -41,6 +41,10 @@ const AppBarNavTemplateWrapper = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
+  background-image: url('../src/assets/images/Background-simple.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `
 
 const ChildrenWrapper = styled.div<Pick<AppBarTemplateProps, 'hasSpacing' | 'hasPadding'>>`
