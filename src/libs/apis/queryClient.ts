20180405db9574dd8b53054c5d782182165441c4
Eugene Kim <eugene02876@gmail.com>
2023-09-26 15:05:08 +0900

    fix : 이상한 post id로 접근하면 메인으로 리다이렉트

--- src/libs/apis/queryClient.ts
+++ src/libs/apis/queryClient.ts
@@ -1,5 +1,6 @@
 import { QueryClient } from '@tanstack/react-query'
-
+import { QueryCache } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
 /**
  * 전역 QueryClientProvider에서 사용되는 QueryClient입니다.
  */
@@ -11,4 +12,10 @@ export const queryClient = new QueryClient({
       refetchOnMount: false,
     },
   },
+  queryCache: new QueryCache({
+    onError: () => {
+      alert('에러 발생! 올바른 경로로 서비스를 이용해주세요.')
+      window.location.href = '/'
+    },
+  }),
 })
