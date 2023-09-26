ef8cfb246d19eda9f8f7d425deb49a0b20c5fbbd
Eugene Kim <eugene02876@gmail.com>
2023-09-26 18:32:09 +0900

    fix: 채널명 보이게끔 router 수정

--- src/pages/postList/index.tsx
+++ src/pages/postList/index.tsx
@@ -1,16 +1,32 @@
+import { useQuery } from '@tanstack/react-query'
 import { useAtomValue } from 'jotai'
 import { Suspense } from 'react'
+import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 import Loading from '@/components/common/loading'
 import AppBarNavTemplate from '@/components/layouts/AppBarNavTemplate'
+import { ChannelApi } from '@/libs/apis/channel/ChannelApi'
 import { urlAtom } from '@/libs/store/urlAtom'
 import PostListPage from '@/pages/postList/[channelId]'
 import PostDetailPage from '@/pages/postList/[postId]'
 import EditPostPage from '@/pages/postList/EditPost'
 import NewPostPage from '@/pages/postList/NewPost'
-
 const PostRouter = () => {
+  const setChannelName = () => {
+    const channelID = useLocation().pathname.split('/')[2]
+    if (channelID) {
+      const { data } = useQuery(['channels'], () => ChannelApi.GET_CHANNEL(), {
+        cacheTime: 0,
+      })
+      const name = data?.filter((data) => data._id === channelID)
+      if (name) {
+        return name[0].name
+      }
+    }
+    return '게시글 보기'
+  }
   const urlData = useAtomValue(urlAtom)
   return (
     <Suspense fallback={<Loading />}>
@@ -18,7 +34,7 @@ const PostRouter = () => {
         <Route
           path={':channelId/*'}
           element={
-            <AppBarNavTemplate hasNav={true} backurl={' '}>
+            <AppBarNavTemplate hasNav={true} backurl={' '} title={setChannelName()}>
               <PostListPage />
             </AppBarNavTemplate>
           }
